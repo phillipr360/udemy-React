0e5272448e4495ba01ae887738d8f184b92b2ed5
Phillip Reisman <phillipr360@Phillips-MBP.hsd1.ma.comcast.net>
2019-05-24 23:08:26 -0400

    Dynamic ingredient list

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -3,13 +3,26 @@ import Ingredient from './Ingredient/Ingredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
+  let ingredientList = Object.keys(props.ingredients)
+    .map(ingKey => {
+      return [...Array(props.ingredients[ingKey])].map((_, i) => {
+        return <Ingredient key={ingKey + i} type={ingKey} />
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  
+  if (ingredientList.length === 0) {
+    ingredientList = <p>Please start adding ingredients!</p>
+  }
+  
+  console.log(ingredientList);
+  
   return (
     <div className={classes.Burger}>
       <Ingredient type="bread-top" />
-      <Ingredient type="salad" />
-      <Ingredient type="bacon" />
-      <Ingredient type="cheese" />
-      <Ingredient type="meat" />
+      {ingredientList}
       <Ingredient type="bread-bottom" />
     </div>
   );
