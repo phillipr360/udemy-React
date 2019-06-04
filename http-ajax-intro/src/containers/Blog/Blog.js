f437fb51b02f0f0d98feb80af48e2d0a79639ba4
Phillip Reisman <phillipr360@Phillips-MBP.hsd1.ma.comcast.net>
2019-06-04 07:51:34 -0400

    NavLinks with styling

--- http-ajax-intro/src/containers/Blog/Blog.js
+++ http-ajax-intro/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import FullPost from './FullPost/FullPost';
@@ -13,15 +13,27 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><Link to="/">Home</Link></li>
               <li>
-                <Link to={{
+                <NavLink
+                  to="/"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: "#FA932F",
+                    textDecoration: "underline"
+                  }}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to={{
                   pathname: "/new-post",
                   hash: "#submit",
                   search: "?quick-submit=true"
                 }}>
                   New Post
-                </Link>
+                </NavLink>
               </li>
             </ul>
           </nav>
